d2883eb98c9f782f12f35159d7b42f54902d83e8
Ivan Carlos de Oliveira Santos <104778982+Eklezyarez@users.noreply.github.com>
2022-09-21 08:20:14 -0300

    Update CloudParkingApplicationTests.java

--- src/test/java/com/dio/cloudparking/CloudParkingApplicationTests.java
+++ src/test/java/com/dio/cloudparking/CloudParkingApplicationTests.java
@@ -3,7 +3,6 @@ package com.dio.cloudparking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class CloudParkingApplicationTests {
   @Test
