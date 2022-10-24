f2fc58b932acc3ddba384e80b8aa4a960a4dd79d
Ivan Carlos de Oliveira Santos <104778982+Eklezyarez@users.noreply.github.com>
2022-10-24 08:05:26 -0300

    Update AbstractContainerBase.java

--- src/test/java/com/dio/cloudparking/controller/AbstractContainerBase.java
+++ src/test/java/com/dio/cloudparking/controller/AbstractContainerBase.java
@@ -4,7 +4,6 @@ import org.testcontainers.containers.PostgreSQLContainer;
 public abstract class AbstractContainerBase {
 
     static final PostgreSQLContainer POSTGRE_SQL_CONTAINER;
-
     static {
         POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:10-alpine");
         POSTGRE_SQL_CONTAINER.start();
